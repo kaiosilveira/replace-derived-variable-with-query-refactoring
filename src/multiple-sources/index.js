5c2466b4a5874b4cd9754995799815f6cf5bc768
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-31 19:14:00 +0100

    multiple-srcs: introduce `calculatedProduction` getter

--- src/multiple-sources/index.js
+++ src/multiple-sources/index.js
@@ -9,6 +9,10 @@ export class ProductionPlan {
     return this._initialProduction + this._productionAccumulator;
   }
 
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
     this._productionAccumulator += anAdjustment.amount;
