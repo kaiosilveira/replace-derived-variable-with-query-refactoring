fb1df73e53c53d4ed02adbba95184eb6e277b00c
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-31 19:01:33 +0100

    single-src: introduce `calculatedProduction` getter

--- src/single-source/index.js
+++ src/single-source/index.js
@@ -8,6 +8,10 @@ export class ProductionPlan {
     return this._production;
   }
 
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
     this._production += anAdjustment.amount;
